64e845ac59bbd7af169b6e183586302c2fe3fa0c
haninasyed <sage5860@gmail.com>
2020-09-18 18:56:41 -0700

    master changed to main

---
+++ src/test/java/com/lambdaschool/secretfamilyrecipe/SecretfamilyrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.secretfamilyrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretfamilyrecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
