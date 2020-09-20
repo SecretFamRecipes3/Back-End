b1f0f2d42af020033e64dbb08dac1f550292d26d
haninasyed <sage5860@gmail.com>
2020-09-19 21:14:06 -0700

    most of the imports fixed but still some left

--- src/main/java/com/lambdaschool/secretfamilyrecipe/models/Role.java
+++ src/main/java/com/lambdaschool/secretfamilyrecipe/models/Role.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "roles")
--- src/main/java/com/lambdaschool/secretfamilyrecipe/services/SecurityUserServiceImpl.java
+++ src/main/java/com/lambdaschool/secretfamilyrecipe/services/SecurityUserServiceImpl.java
@@ -5,11 +5,12 @@ import com.lambdaschool.secretfamilyrecipe.models.User;
 import com.lambdaschool.secretfamilyrecipe.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service(value = "securityUserService")
-public class SecurityUserServiceImpl {
+public class SecurityUserServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepository userrepos;
